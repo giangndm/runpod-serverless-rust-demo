b5afa1bfb463e3473b5e846445f19b8111c7388d
Giang Minh <giang.ndm@gmail.com>
2026-03-06 14:14:49 +0700

    fix endpoint list parsing and publish Docker image to GHCR

--- src/bin/runpod_endpoint.rs
+++ src/bin/runpod_endpoint.rs
@@ -1,16 +1,38 @@
-use runpod_sdk::model::ListEndpointsQuery;
-use runpod_sdk::service::EndpointsService;
-use runpod_sdk::{Result, RunpodClient};
+use reqwest::Client;
+use runpod_sdk::RunpodConfig;
+use runpod_sdk::model::{ComputeType, ListEndpointsQuery};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct EndpointSummary {
+    id: String,
+    name: Option<String>,
+    #[serde(default)]
+    compute_type: Option<ComputeType>,
+    #[serde(default)]
+    workers_min: i32,
+    #[serde(default)]
+    workers_max: i32,
+}
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let client = RunpodClient::from_env()?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = RunpodConfig::from_env()?;
+    let client = Client::builder().timeout(config.timeout()).build()?;
+    let url = format!("{}/endpoints", config.rest_url().trim_end_matches('/'));
 
     let endpoints = client
-        .list_endpoints(ListEndpointsQuery {
+        .get(url)
+        .bearer_auth(config.api_key())
+        .query(&ListEndpointsQuery {
             include_template: Some(false),
             include_workers: Some(false),
         })
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Vec<EndpointSummary>>()
         .await?;
 
     if endpoints.is_empty() {
@@ -21,11 +43,15 @@ async fn main() -> Result<()> {
     println!("id\tname\tcompute_type\tworkers(min/max)");
     for endpoint in endpoints {
         let name = endpoint.name.unwrap_or_else(|| "-".to_string());
+        let compute_type = endpoint
+            .compute_type
+            .map(|value| format!("{value:?}"))
+            .unwrap_or_else(|| "-".to_string());
         println!(
-            "{}\t{}\t{:?}\t{}/{}",
+            "{}\t{}\t{}\t{}/{}",
             endpoint.id,
             name,
-            endpoint.compute_type,
+            compute_type,
             endpoint.workers_min,
             endpoint.workers_max,
         );
